a06d08a486d5a312669cbd01131c148648762d8f
NanWu <57171300+NanWu00@users.noreply.github.com>
2025-11-23 11:15:31 +0800

    Update change_ipv6_with_prompt.sh

--- change_ipv6_with_prompt.sh
+++ change_ipv6_with_prompt.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-# 网络接口名称
-INTERFACE="enp0s3"
+# 下载并保存脚本到 /usr/local/bin/change_ipv6
+echo "正在下载 change_ipv6 脚本..."
+curl -sSL https://raw.githubusercontent.com/NanWu00/acme-onekey/refs/heads/main/change_ipv6_with_prompt.sh -o /usr/local/bin/change_ipv6
 
-# 提示用户输入新的IPv6地址
+# 给下载的脚本添加执行权限
+echo "为脚本添加执行权限..."
+sudo chmod +x /usr/local/bin/change_ipv6
+
+# 创建快捷命令 (使 change ipv6 可以执行)
+if ! grep -q "change ipv6" ~/.bashrc; then
+    echo 'alias "change ipv6"="/usr/local/bin/change_ipv6"' >> ~/.bashrc
+    echo "快捷命令已创建：change ipv6"
+else
+    echo "快捷命令已经存在：change ipv6"
+fi
+
+# 让 alias 立即生效
+source ~/.bashrc
+
+# 提示用户输入新的 IPv6 地址
 echo "请输入新的 IPv6 地址（前缀保持 /128）："
 read NEW_IPV6
 
@@ -12,6 +28,9 @@ if [[ -z "$NEW_IPV6" ]]; then
     echo "错误：IPv6 地址不能为空！"
     exit 1
 fi
+
+# 网络接口名称
+INTERFACE="enp0s3"
 
 # 删除当前的 IPv6 地址（确保是旧的地址）
 OLD_IPV6=$(ip -6 addr show dev $INTERFACE | grep "inet6" | awk '{print $2}')
